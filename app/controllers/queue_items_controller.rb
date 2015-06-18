230a519c180bcbba4be63ea157aa92e5980e955b
Eric Tillberg <eric.tillberg@gmail.com>
2015-06-17 22:06:24 -0400

    another small fix

--- app/controllers/queue_items_controller.rb
+++ app/controllers/queue_items_controller.rb
@@ -56,12 +56,10 @@ class QueueItemsController < ApplicationController
   end
 
   def update_queue_items
-    if QueueItem.count > 0
-      ActiveRecord::Base.transaction do
-        params[:queue_items].each do |queue_item_data|
-          queue_item = QueueItem.find(queue_item_data[:id])
-          queue_item.update_attributes!(position: queue_item_data[:position]) if queue_item.user == current_user
-        end
+    ActiveRecord::Base.transaction do
+      params[:queue_items].each do |queue_item_data|
+        queue_item = QueueItem.find(queue_item_data[:id])
+        queue_item.update_attributes!(position: queue_item_data[:position]) if queue_item.user == current_user
       end
     end
   end
